63251d93a9ba57cf6e7ac94b9734beb13aca31c3
dunchiii <dhruvansht@gmail.com>
2022-11-13 19:54:16 +0530

    Final TestNG lecture commit

--- src/test/java/dhruvanshtanwar/tests/ErrorValidationsTest.java
+++ src/test/java/dhruvanshtanwar/tests/ErrorValidationsTest.java
@@ -7,32 +7,30 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import dhruvanshtanwar.TestComponents.BaseTest;
+import dhruvanshtanwar.TestComponents.*;
 import dhruvanshtanwar.pageobjects.CartPage;
 import dhruvanshtanwar.pageobjects.ProductCatalogue;
 
 public class ErrorValidationsTest extends BaseTest {
-	
-	//This class is for all the Negative Test Cases
 
-	@Test(groups = {"ErrorHandling"})
-	public void loginErrorValidation() throws InterruptedException, IOException {
+	// This class is for all the Negative Test Cases
+
+	@Test(groups = { "ErrorHandling" }, retryAnalyzer = Retry.class)
+	public void loginErrorValidation() throws IOException, InterruptedException {
 
 //		String productName = "ZARA COAT 3";
-		
-		//Passing wrong email and password
+
+		// Passing wrong email and password
 		landingPage.loginApplication("dhruvanht@gmail.com", "Hardrockcafe1");
 		Assert.assertEquals("Incorrect email  passsword.", landingPage.getErrorMessage());
-		//#toast-container > div
-		//class = ng-tns-c4-7 ng-star-inserted ng-trigger ng-trigger-flyInOut ngx-toastr toast-error
 		System.out.println("Incorrect email or password.");
 	}
-	
+
 	@Test
 	public void productErrorValidation() throws InterruptedException, IOException {
 
 		String productName = "ZARA COAT 3";
-		//LandingPage landingPage = launchApplication();
+		// LandingPage landingPage = launchApplication();
 		ProductCatalogue productCatalogue = landingPage.loginApplication("dhruvansht@gmail.com", "Hardrockcafe1.");
 		List<WebElement> products = productCatalogue.getProductList();
 		CartPage cartPage = productCatalogue.addProductToCart(productName);
@@ -40,5 +38,5 @@ public class ErrorValidationsTest extends BaseTest {
 		Boolean match = cartPage.verifyProductsDisplay("ZARA COAT 33");
 		Assert.assertFalse(match);
 	}
-	
+
 }
